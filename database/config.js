83a730fe96c5d0ddb857d846a958866e72a803b9
dienorseman <edgar.elps@gmail.com>
2023-06-12 14:11:20 -0600

    Env example added

--- database/config.js
+++ database/config.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/coffe')
+        await mongoose.connect(process.env.DB_HOST)
         const db = mongoose.connection;
         console.log(`Connected to database ${db.name} on host ${db.host}`);
     } catch (error) {
