59b7a7fa77b1e030f924d5c30f622f4a14a86df0
dienorseman <edgar.elps@gmail.com>
2023-06-12 14:07:26 -0600

    "console log database connection "

--- database/config.js
+++ database/config.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 const dbConnection = async () => {
     try {
         await mongoose.connect('mongodb://127.0.0.1:27017/coffe')
-        console.log('database connected');
+        const db = mongoose.connection;
+        console.log(`Connected to database ${db.name} on host ${db.host}`);
     } catch (error) {
         console.log(error);
     }
